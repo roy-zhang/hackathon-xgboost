72c56d2f27bbaed66f31aa0667c3730da24d2aca
Roy Zhang <roy.zhang@nike.com>
2019-05-20 16:30:00 -0700

    some data pruning using known counterfeit

--- src/hackathon/core.clj
+++ src/hackathon/core.clj
@@ -5,8 +5,11 @@
 (def csv1 "feb1_may3.csv")
 (def csv2 "nike_locations.csv")
 (def csv3 "anomaly-locs.csv")
+(def csv4 "clean_dataset.csv")
+(def csv5 "bad-ip-events.csv")
 
-(def events (file/csv-data->maps (file/read-csv csv1)))
+(def eventsOld (file/csv-data->maps (file/read-csv csv1)))
+(def events (file/csv-data->maps (file/read-csv csv4)))
 (def nike-locations (set (rest (map format/convert-nums (file/read-csv csv2)))))
 (def anomalys  (set (rest (map format/convert-nums (file/read-csv csv3)))))
 
@@ -17,3 +20,24 @@
 
 ;; (def bad (match-events-locs events anomalys)) # count 71048
 ;; (def good (match-events-locs events nike-locations))) # count 2180
+;; (def zeros (match-events-locs events #{[0 0}))) # 22984
+
+
+(defn known-bad-locations [events]
+  (->> events
+    (filter #(= 1.0 (:label %)))
+    (map #(vec (list (:lat %) (:lng %))))
+    (remove #(= [0.0 0.0] %))
+    set))
+
+(def new-bad-events (match-events-locs (filter #(= 0.0 (:label %)) events)
+                                       (known-bad-locations events)))
+
+(defn switch-label [events kids]
+  (map (fn [{:keys [kid] :as event}]
+         (if (contains? kids kid)
+           (assoc event :label 1.0)
+           event))
+       events))
+
+(def new-events (switch-label events (set (map :kid new-bad-events))))
